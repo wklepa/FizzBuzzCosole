396e388f21b8e4d6c85cdd77c719f7dce348695f
Wojciech Klepacki <wklepa@outlook.com>
2022-06-30 11:12:46 +1000

    Added additional conditions checking and some comments

--- FizzBuzzCosole/Program.cs
+++ FizzBuzzCosole/Program.cs
@@ -13,7 +13,7 @@
             // Capture "divisible by five" condition
             else if (i % 3 == 0)
                 Console.WriteLine(i.ToString() + " " + "Buzz");
-            // Print the nuber if none of the criterias are met
+            // Print the nuber if none of the conditions are met
             else
                 Console.WriteLine(i.ToString());
         }
@@ -21,14 +21,20 @@
     // Call the defined method
     static void Main()
     {
-        Console.WriteLine("Enter number to end rhe range: ");
+        Console.WriteLine("Enter number to end rhe range:");
+        // Converting and checking the input
         try
         {
             int inpNumber = Int32.Parse(Console.ReadLine());
             if (inpNumber == 0)
                 Console.WriteLine("Number must be greater than '0'");
             else
-                checkDivisibility(Math.Abs(inpNumber));
+            {
+                if (inpNumber < 0)
+                    Console.WriteLine("The '" + inpNumber.ToString() + "' converted to the absolute value.");
+                    inpNumber = Math.Abs(inpNumber);
+            }
+                checkDivisibility(inpNumber);
         }
         catch (FormatException)
         {
